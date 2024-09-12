de7961ced78dcf807cb6e30dd50afacda2441bb5
Manuel Gomez <jmgc@virket.com>
2024-09-12 09:14:03 -0600

    Preparar cliente para enviar JWT

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { connectToServer } from './socket-client.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>WebSocket - Client</h1>
+    <h2>WebSocket - Client</h2>
+    <input id="jwt-token" type="text" placeholder="JWT Token">
+    <button id="btn-connect">Connect</button>
+    <br />
     <span id="server-status">offline</span>
 
     <ul id="clients-ul">
@@ -20,4 +23,14 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
-connectToServer();
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+btnConnect.addEventListener('click', () => {
+
+  if ( jwtToken.value.trim().length <= 0 ) return alert('Enter a valid JWT Token');
+  connectToServer(jwtToken.value);
+
+});
+
+// connectToServer();
